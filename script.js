5e1b3a38495430756c8f20598aa9caf28d2a21ba
Arquero <PC@arch.linux>
2024-09-07 22:29:19 -0400

    feat: added navbar and hacker-like design

--- script.js
+++ script.js
@@ -26,23 +26,32 @@ function delete_task() {
 function insertDiv(val) {
 	const task = document.createElement("p");
 	task.style.textAlign = "center"
+	task.style.margin = "auto"
 	const text = document.createTextNode(val);
 	task.appendChild(text);
 
 
 	const task_container = document.createElement('div')
+	const inner_container = document.createElement('div')
+	inner_container.style.margin = "0 auto 0 auto"
+	inner_container.style.display = "flex"
+	inner_container.style.flexDirection = "row"
+	inner_container.style.flexWrap = "wrap"
 	var svg = document.createElementNS("http://www.w3.org/2000/svg", 'svg');
 	var newElement = document.createElementNS("http://www.w3.org/2000/svg", 'path');
 	newElement.setAttribute("d", "M9 9L15 15M15 9L9 15");
-	newElement.style.stroke = "red";
+	newElement.style.stroke = "green";
 	newElement.style.strokeWidth = "2px";
-	svg.appendChild(newElement);
-	svg.style.width = "30px"
-	svg.style.height = "30px"
-	svg.onclick = function() { }
+	newElement.style.cursor = "pointer";
 
-	task_container.appendChild(svg)
-	task_container.appendChild(task)
+	svg.appendChild(newElement);
+	svg.onclick = function() { }
+	svg.setAttribute("viewBox", "5 5 14 14");
+	svg.setAttribute("width", "30px");
+
+	inner_container.appendChild(svg)
+	inner_container.appendChild(task)
+	task_container.appendChild(inner_container)
 	task_container.style.cssText = `
 		background-color: green;
 		color: black;
@@ -50,6 +59,7 @@ function insertDiv(val) {
 		flex-direction: row;
 		margin-top: 20px;
 		margin-bottom: 20px;
+		margin-left: auto;
 	`
 	const results = document.getElementById("results")
 	results.appendChild(task_container)
